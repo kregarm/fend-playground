58a0e88eada5f3f6c6106880857b1b408b332948
Martin Kregar <martinkregar@pop-os.localdomain>
2019-12-14 19:41:51 +0100

    added bundle-analyzer-plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 
 module.exports = {
     mode: 'development',
@@ -18,6 +20,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: './src/client/views/index.html',
             filename: './index.html'
-        })
+        }),
+        new BundleAnalyzerPlugin()
     ]
 };
